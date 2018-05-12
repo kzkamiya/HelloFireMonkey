ecae5f3de734c7036a9255f41f8446779ff386df
kzkamiya@gmail.com <kzkamiya@gmail.com>
2018-05-12 16:07:28 +0900

    Add

---
+++ frmMain.pas
@@ -0,0 +1,28 @@
+unit frmMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Ani,
+  FMX.Objects;
+
+type
+  TForm1 = class(TForm)
+    Rectangle1: TRectangle;
+    FloatAnimation1: TFloatAnimation;
+    ColorAnimation1: TColorAnimation;
+  private
+    { private êÈåæ }
+  public
+    { public êÈåæ }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
